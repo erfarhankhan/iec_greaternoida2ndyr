47ec82ed10ff06069f3fc09d198c10f4347d0db9
erfarhankhan <hfkhan991@gail.com>
2025-11-19 10:09:55 +0530

    2ndpush

---
+++ js_demo/INTRO.js
@@ -0,0 +1,22 @@
+/* 
+What Can JavaScript Do?
+JavaScript is the programming language of the web.
+
+It can calculate, manipulate and validate data.
+
+It can update and change both HTML and CSS.
+*/
+
+
+
+/* 
+    JavaScript in <head> or <body>
+You can place any number of scripts in an HTML document.
+
+Scripts can be placed in the <body>, or in the <head> section of an HTML page, or in both.
+
+JavaScript in <head>
+In this example, a JavaScript function is placed in the <head> section of an HTML page.
+
+The function is invoked (called) when a button is clicked:
+*/
